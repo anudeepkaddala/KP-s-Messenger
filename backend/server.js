cc850f63c818c9e58289b9f6e8086b575ab6b91d
Anudeep <anudeepkaddala@gmail.com>
2024-06-01 06:30:11 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT=process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser());
@@ -16,10 +19,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
 
-//app.get('/',(req,res) => {
-    // root route http://localhost:5000/
-//    res.send('Hello guys');
-//});
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,() => {
     connectToMongoDB();
