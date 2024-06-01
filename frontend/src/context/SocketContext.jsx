12237636f93959a32bf40387219d0df6b5a9c607
Anudeep <anudeepkaddala@gmail.com>
2024-06-01 06:57:20 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,11 +15,11 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5001", {
+            const socket = io("https://chat-app-yt.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
-			}); // Changed to match your backend port
+			}); 
 
             setSocket(socket);
 
